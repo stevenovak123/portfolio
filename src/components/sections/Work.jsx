1ec064dafb4dbc7fc1d05900c2458867a7d6b77c
stevenovak123 <stevenovak0802@gmail.com>
2025-05-31 23:22:45 +0100

    added work with dummy data

--- src/components/sections/Work.jsx
+++ src/components/sections/Work.jsx
@@ -1,7 +1,188 @@
 import React from 'react';
 
 const Work = () => {
-  return <div id='work'>Work</div>;
+  return (
+    <section
+      id='work'
+      className='min-h-screen flex items-center justify-center py-20 '
+    >
+      <div className='max-w-5xl mx-auto px-4'>
+        <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>
+          Featured Work.
+        </h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'>
+            <h3 className='text-xl font-bold mb-2'>
+              Activity Recommendation Service
+            </h3>
+            <p className='text-gray-400 mb-3'>
+              Recommends an activity with City as input and then using the
+              Weather and other preferences give suggestions on indoor and
+              outdoor activies
+            </p>
+            <div>
+              {['SpringBoot', 'Swagger', 'Java'].map((tech, key) => (
+                <span
+                  className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'
+                  key={key}
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+            <div className=' flex justify-between items-center'>
+              <a
+                className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                href='https://github.com/stevenovak123/SOA_assignment'
+              >
+                View Code 🡢
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
